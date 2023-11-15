e121e36dd786cdbcda98ee9b8a661acacdd62cd2
AkhilAjith2 <104864079+AkhilAjith2@users.noreply.github.com>
2023-11-15 20:31:18 +0100

    Add files via upload

---
+++ ImageSlideshow/MainWindow.xaml.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<string> AvailableSlideshowEffects { get; set; }
+        public string SelectedSlideshowEffect { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this; // Set the DataContext to the MainWindow instance
+
+            // Populate the AvailableSlideshowEffects collection
+            AvailableSlideshowEffects = new ObservableCollection<string>
+            {
+                "Horizontal Effect",
+                "Opacity Effect",
+                "Vertical Effect"
+            };
+        }
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            LoadingDrivers();
+        }
+
+        private string selectedFolderPath;
+        private void OpenFolderMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+            DialogResult result = dialog.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                selectedFolderPath = dialog.SelectedPath;
+                string[] imageFiles = Directory.GetFiles(selectedFolderPath, "*.jpg");
+                ImageListView.ItemsSource = imageFiles;
+            }
+        }
+
+        private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void AboutMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.MessageBox.Show("Image Explorer Application\nVersion 1.0\n\n LAB WPF2 GRADED", "About");
+        }
+
+        //https://learn.microsoft.com/en-us/dotnet/desktop/wpf/controls/how-to-find-a-treeviewitem-in-a-treeview?view=netframeworkdesktop-4.8
+
+        private void LoadingDrivers()
+        {
+            ExplorerTreeView.Items.Clear();
+
+            string[] partitions = Directory.GetLogicalDrives();
+
+            foreach (string partition in partitions)
+            {
+                TreeViewItem partitionItem = new TreeViewItem();
+                partitionItem.Header = partition;
+                partitionItem.Tag = partition;
+                partitionItem.Expanded += Expanded_Drivers;
+                partitionItem.Items.Add(null);
+                ExplorerTreeView.Items.Add(partitionItem);
+
+            }
+        }
+
+        private void Expanded_Drivers(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem partitionItem = (TreeViewItem)sender;
+
+            if (partitionItem.Items.Count == 1 && partitionItem.Items[0] == null)
+            {
+                partitionItem.Items.Clear();
+
+                string partitionPath = (string)partitionItem.Tag;
+
+                string[] directories = Directory.GetDirectories(partitionPath);
+
+                foreach (string directory in directories)
+                {
+                    TreeViewItem directoryItem = new TreeViewItem();
+                    directoryItem.Header = new DirectoryInfo(directory).Name;
+                    directoryItem.Tag = directory;
+                    directoryItem.Expanded += Expanded_Directories;
+                    directoryItem.Items.Add(null);
+                    partitionItem.Items.Add(directoryItem);
+                }
+            }
+        }
+
+        private void Expanded_Directories(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem directoryItem = (TreeViewItem)sender;
+
+            if (directoryItem.Items.Count == 1 && directoryItem.Items[0] == null)
+            {
+                directoryItem.Items.Clear();
+                string directoryPath = (string)directoryItem.Tag;
+                string[] directories = Directory.GetDirectories(directoryPath);
+
+                foreach (string directory in directories)
+                {
+                    TreeViewItem subDirectoryItem = new TreeViewItem();
+                    subDirectoryItem.Header = new DirectoryInfo(directory).Name;
+                    subDirectoryItem.Tag = directory;
+                    subDirectoryItem.Expanded += Expanded_Directories;
+                    subDirectoryItem.Items.Add(null);
+                    directoryItem.Items.Add(subDirectoryItem);
+                }
+
+                string[] imageFiles = Directory.GetFiles(directoryPath, "*.jpg");
+                ImageListView.ItemsSource = imageFiles;
+            }
+        }
+        //https://learn.microsoft.com/en-us/dotnet/api/system.windows.controls.selectionchangedeventargs?view=windowsdesktop-7.0
+        private void ImageListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ImageListView.SelectedItem != null)
+            {
+                string imagePath = (string)ImageListView.SelectedItem;
+                string fileName = System.IO.Path.GetFileName(imagePath);
+                FileInfoTextBlock.Text = $"File Name: {fileName}";
+
+                BitmapImage bitmapImage = new BitmapImage(new Uri(imagePath));
+
+                int width = bitmapImage.PixelWidth;
+                int height = bitmapImage.PixelHeight;
+                FileInfo fileInfo = new FileInfo(imagePath);
+                long fileSize = fileInfo.Length / 1024;
+                WidthTextBlock.Text = $"Width: {width}px";
+                HeightTextBlock.Text = $"Height: {height}px";
+                SizeTextBlock.Text = $"Size: {fileSize} KB";
+
+            }
+            else
+            {
+                FileInfoTextBlock.Text = "No file selected";
+                WidthTextBlock.Text = string.Empty;
+                HeightTextBlock.Text = string.Empty;
+                SizeTextBlock.Text = string.Empty;
+            }
+        }
+
+        private void ApplyTransitionEffects(SlideshowWindow slideshowWindow)
+        {
+            int currentIndex = slideshowWindow.IndexCurr;
+
+            switch (slideshowWindow.SelectedEffect)
+            {
+                case "Horizontal Effect":
+                    slideshowWindow.ApplyHorizontalEffect(currentIndex);
+                    break;
+                case "Vertical Effect":
+                    slideshowWindow.ApplyVerticalEffect(currentIndex);
+                    break;
+                case "Opacity Effect":
+                    slideshowWindow.ApplyOpacityEffect(currentIndex);
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void SlideshowEffectMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Controls.MenuItem menuItem = (System.Windows.Controls.MenuItem)sender;
+            string selectedEffect = menuItem.Header.ToString();
+
+            StartSlideshow(selectedEffect);
+        }
+
+        private void StartSlideshowButton_Click(object sender, RoutedEventArgs e)
+        {
+            string selectedEffect = EffectComboBox.SelectedItem as string;
+            if (selectedEffect != null)
+            {
+                StartSlideshow(selectedEffect);
+            }
+        }
+
+        private void StartSlideshow(string effect)
+        {
+            SlideshowWindow slideshowWindow = new SlideshowWindow(effect);
+
+            slideshowWindow.InitializeSlideshow(selectedFolderPath);
+
+            slideshowWindow.Loaded += (sender, e) =>
+            {
+                ApplyTransitionEffects(slideshowWindow);
+            };
+
+            slideshowWindow.ShowDialog();
+
+        }
+    }
+    //Tutorial
+    public class FileNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string filePath = value as string;
+            if (!string.IsNullOrEmpty(filePath))
+            {
+                return System.IO.Path.GetFileName(filePath);
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
+
---
+++ ImageSlideshow/SlideshowWindow.xaml.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace WPF2
+{
+    /// <summary>
+    /// Interaction logic for SlideshowWindow.xaml
+    /// </summary>
+    public partial class SlideshowWindow : Window
+    {
+        private DispatcherTimer SlideTimer;
+        private bool isPaused = false;
+        public string SelectedEffect { get; set; }
+        public List<Image> Images { get; set; }
+        public int IndexCurr { get; set; }
+
+        public SlideshowWindow(string effect)
+        {
+            InitializeComponent();
+            SelectedEffect = effect;
+            Images = new List<Image>();
+            IndexCurr = 0;
+        }
+
+        public void InitializeSlideshow(string folderPath)
+        {
+            string[] imageFiles = Directory.GetFiles(folderPath, "*.jpg");
+
+            foreach (string imagePath in imageFiles)
+            {
+                BitmapImage imageSource = new BitmapImage();
+                imageSource.BeginInit();
+                imageSource.UriSource = new Uri(imagePath, UriKind.Absolute);
+                imageSource.EndInit();
+
+                Image image = new Image();
+                image.Source = imageSource;
+                image.Tag = Images.Count;
+
+                Images.Add(image);
+                canvas.Children.Add(image); 
+            }
+
+            if (Images.Count > 0)
+            {
+                slideshowImage.Source = Images[0].Source;
+            }
+
+            SlideTimer = new DispatcherTimer();
+            SlideTimer.Interval = TimeSpan.FromSeconds(2);
+
+            SlideTimer.Tick += (sender, e) =>
+            {
+                IndexCurr = (IndexCurr + 1) % Images.Count;
+                slideshowImage.Source = Images[IndexCurr].Source;
+                switch (SelectedEffect)
+                {
+                    case "Horizontal Effect":
+                        ApplyHorizontalEffect(IndexCurr);
+                        break;
+                    case "Vertical Effect":
+                        ApplyVerticalEffect(IndexCurr);
+                        break;
+                    case "Opacity Effect":
+                        ApplyOpacityEffect(IndexCurr);
+                        break;
+                    default:
+                        break;
+                }
+            };
+            SlideTimer.Start();
+        }
+
+
+        public void ApplyHorizontalEffect(int currentIndex)
+        {
+            double Width = canvas.ActualWidth;
+            double initialX = Width;
+            double finalX = 0;
+            try
+            {
+                foreach (Image image in canvas.Children.OfType<Image>())
+                {
+                    if (image.Tag != null && int.TryParse(image.Tag.ToString(), out int index))
+                    {
+                        if (index == currentIndex)
+                        {
+                            DoubleAnimation horizontalAnimation = new DoubleAnimation();
+                            horizontalAnimation.From = initialX;
+                            horizontalAnimation.To = finalX;
+                            horizontalAnimation.Duration = new Duration(TimeSpan.FromSeconds(3));
+
+                            image.BeginAnimation(Canvas.LeftProperty, horizontalAnimation);
+                        }
+                        else if (index > currentIndex)
+                        {
+                            Canvas.SetLeft(image, initialX);
+                        }
+                        else
+                        {
+                            Canvas.SetLeft(image, finalX);
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void ApplyVerticalEffect(int currentIndex)
+        {
+            double canvasHeight = canvas.ActualHeight;
+            double initialY = canvasHeight;
+            double finalY = 0;
+
+            foreach (Image image in canvas.Children.OfType<Image>())
+            {
+                if (image.Tag != null && int.TryParse(image.Tag.ToString(), out int index))
+                {
+                    if (index == currentIndex)
+                    {
+                        DoubleAnimation verticalAnimation = new DoubleAnimation();
+                        verticalAnimation.From = initialY;
+                        verticalAnimation.To = finalY;
+                        verticalAnimation.Duration = new Duration(TimeSpan.FromSeconds(3));
+
+                        image.BeginAnimation(Canvas.TopProperty, verticalAnimation);
+                    }
+                    else if (index > currentIndex)
+                    {
+                        Canvas.SetTop(image, initialY);
+                    }
+                    else
+                    {
+                        Canvas.SetTop(image, finalY);
+                    }
+                }
+            }
+        }
+
+        public void ApplyOpacityEffect(int currentIndex)
+        {
+            foreach (Image image in canvas.Children.OfType<Image>())
+            {
+                if (image.Tag != null && int.TryParse(image.Tag.ToString(), out int index))
+                {
+                    if (index == currentIndex)
+                    {
+                        DoubleAnimation opacityAnimation = new DoubleAnimation();
+                        opacityAnimation.From = 0;
+                        opacityAnimation.To = 1;
+                        opacityAnimation.Duration = new Duration(TimeSpan.FromSeconds(1));
+
+                        image.BeginAnimation(UIElement.OpacityProperty, opacityAnimation);
+                    }
+                    else
+                    {
+                        image.Opacity = 0;
+                    }
+                }
+            }
+        }
+
+        private void PlayPause_Click(object sender, RoutedEventArgs e)
+        {
+            if (isPaused)
+            {
+                SlideTimer.Start();
+                isPaused = false;
+            }
+            else
+            {
+                SlideTimer.Stop();
+                isPaused = true;
+            }
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
